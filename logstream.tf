bfaac713bbd2b65ec5111ee3e42144827568e32a
Roberto Dedoro <34625241+mirobertod@users.noreply.github.com>
2019-10-22 08:28:21 +0200

    Update logstream.tf

--- logstream.tf
+++ logstream.tf
@@ -10,6 +10,6 @@ resource "aws_lambda_permission" "cloudwatch_allow" {
   statement_id = "cloudwatch_allow"
   action = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.lambda-logstream.arn}"
-  principal = "logs.eu-west-1.amazonaws.com"
+  principal = "logs.${data.aws_region.current.name}.amazonaws.com"
   source_arn = "${aws_cloudwatch_log_group.lambda-task-loggroup.arn}"
 }
